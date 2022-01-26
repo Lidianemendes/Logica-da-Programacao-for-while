e342c2caea5cf8a9f2e490ec3c9e32e31cca8909
lidianemendes <lidianemendes98@yahoo.com.br>
2022-01-26 00:00:15 -0300

    lógica da programação

---
+++ script.js
@@ -0,0 +1,22 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(let x = 1; x <= 10; x++)
+console.log(x)
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+for(let x = 10; x >= 0; x--){
+    console.log(x);
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let z = 1; z <= 100; z +=2){
+    console.log(z)
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let y = 0; y <= 100; y +=2){
+    console.log(y)
+}
